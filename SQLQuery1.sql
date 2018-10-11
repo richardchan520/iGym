2aeabdf50f2bd0aea56941eff1a9af0212672896
richardchan520 <43195610+richardchan520@users.noreply.github.com>
2018-10-12 01:06:01 +1100

    iGym

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Forum] (
+    [PostID]     INT            NOT NULL,
+    [UserID]     INT            IDENTITY (1, 1) NOT NULL,
+    [PostDate]   DATETIME           NULL,
+    [Postinfo]   NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PostID] ASC)
+);
